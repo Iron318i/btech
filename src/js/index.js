9c64c86c95d7f4d9ab322015f6f2e64a382a717d
Iron318i <iron318i@gmail.com>
2019-11-27 14:50:29 +0300

    Typography

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,5 @@ import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
 
-jQuery(function() {
-  jQuery("body").css("color", "blue");
+jQuery(function () {
 });
