81792ba55fa922133091495072f8cd9b796c8822
Iron318i <iron318i@gmail.com>
2019-11-29 16:50:11 +0300

    Agreement to sell - payment-options (swither + animation)

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,11 @@ import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
 
-jQuery(function () {
-});
++function ($) {
+// How you want to be Paid tempory script
+    $('.payment').on('click', function () {
+	$('.payment-options .payment').removeClass('active');
+	$(this).addClass('active');
+    });
+
+}(jQuery);
