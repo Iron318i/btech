f788244c7707c44f402d49d4bed57ac84d313f52
Iron318i <iron318i@gmail.com>
2019-11-29 16:51:47 +0300

    Agreement to sell - script fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import popper from "popper.js";
 import bootstrap from "bootstrap";
 
 +function ($) {
-// How you want to be Paid tempory script
+// How you want to be Paid temporary script
     $('.payment').on('click', function () {
 	$('.payment-options .payment').removeClass('active');
 	$(this).addClass('active');
