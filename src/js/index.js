35fca4953986c5686cd1079ee2c1a983485f5928
Iron318i <iron318i@gmail.com>
2019-11-29 17:44:45 +0300

    bootstrap-touchspin install

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import jQuery from "jquery";
 import popper from "popper.js";
 import bootstrap from "bootstrap";
+//import bootstrap-touchspin from "bootstrap-touchspin";
+
 
 +function ($) {
 // How you want to be Paid temporary script
