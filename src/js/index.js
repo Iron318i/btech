547b6d7219d462b801925f5fbedfac7cf95b6eec
Iron318i <iron318i@gmail.com>
2019-11-30 16:21:45 +0300

    add same-height js

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,8 @@ import popper from "popper.js";
 import bootstrap from "bootstrap";
 //import bootstrap-touchspin from "bootstrap-touchspin";
 
+var SameHeight = require('same-height');
+SameHeight.init('.same-height-md', "screen and (min-width: 768px)");
 
 +function ($) {
 // How you want to be Paid temporary script
