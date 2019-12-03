99c11a7c126ca99c79dcc2867754855d1b4f7b5a
Iron318i <iron318i@gmail.com>
2019-12-03 10:59:03 +0300

    Single post - video+comments

--- src/js/index.js
+++ src/js/index.js
@@ -14,4 +14,13 @@ SameHeight.init('.same-height-md', "screen and (min-width: 768px)");
 	$(this).addClass('active');
     });
 
+    $('.video').parent().click(function () {
+	if ($(this).children(".video").get(0).paused) {
+	    $(this).children(".video").get(0).play();
+	    $(this).children(".playpause").fadeOut();
+	} else {
+	    $(this).children(".video").get(0).pause();
+	    $(this).children(".playpause").fadeIn();
+	}
+    });
 }(jQuery);
